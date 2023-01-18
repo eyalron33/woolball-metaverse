c8076596e0c06d7bddc93416e7db47a22219402c
neiman <eyalron33@gmail.com>
2023-01-18 16:36:07 +0100

    update metaverse

---
+++ tilesets/overworld.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="overworld" tilewidth="32" tileheight="32" tilecount="40" columns="10">
+ <image source="overworld.png" width="336" height="144"/>
+</tileset>
